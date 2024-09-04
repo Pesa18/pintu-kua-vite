83b44f1b9c83018f2829de5e90b56ee471f12ce6
DESKTOP-H11A5IA\dexter18 <asepalamin42@gmail.com>
2024-09-04 15:55:42 +0700

    asd

--- src/Auth/LoginFetch.jsx
+++ src/Auth/LoginFetch.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const checkEmail = async (email) => {
   try {
     const response = await axios.post(
-      `${import.meta.env.VITE_API_URL}/ceklogin`,
+      `${import.meta.env.VITE_API_URL}/auth/ceklogin`,
       {
         email,
       },
